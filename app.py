f4f073eb37ec8abeba26ac7ad39adde2923fadfb
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-24 18:54:30 +0100

    basic model implemented

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ from playhouse.db_url import connect
 DB = connect(os.environ.get('DATABASE_URL') or 'sqlite:///predictions.db')
 
 class Prediction(Model):
-    observation_id = IntegerField(unique=True)
+    observation_id = TextField(unique=True)
     observation = TextField()
+    prediction = FloatField()
     proba = FloatField()
     true_class = IntegerField(null=True)
 
@@ -52,6 +53,33 @@ with open(os.path.join(PATH_TO_MODEL, 'dtypes.pickle'), 'rb') as fh:
 # End model un-pickling
 ########################################
 
+########################################
+# Begin field verification
+def verify_data_types(data):
+    expected_types = {
+        "observation_id": [str],
+        "Type": [str],
+        "Date": [str],
+        "Part of a policing operation": [bool],
+        "Latitude": [float, int],
+        "Longitude": [float, int],
+        "Gender": [str],
+        "Age range": [str],
+        "Officer-defined ethnicity": [str],
+        "Legislation": [str],
+        "Object of search": [str],
+        "station": [str]
+    }
+    for col, expected_type in expected_types.items():
+        if col not in data:
+            return (True, {'error': f"{col} column not found"})
+        actual_type = type(data[col])
+        if actual_type not in expected_type:
+            return (True, {'error': f"{col} column has wrong data type. Expected {expected_type}, got {actual_type}"})
+    return (False, "All data types are correct")
+
+# End field verification
+########################################
 
 ########################################
 # Begin webserver stuff
@@ -59,24 +87,34 @@ with open(os.path.join(PATH_TO_MODEL, 'dtypes.pickle'), 'rb') as fh:
 app = Flask(__name__)
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/should_search', methods=['POST'])
 def predict():
     # Flask provides a deserialization convenience function called
     # get_json that will work if the mimetype is application/json.
     obs_dict = request.get_json()
-    _id = obs_dict['id']
-    observation = obs_dict['observation']
+
+    is_error, error_msg = verify_data_types(obs_dict)
+    if is_error:
+        response = {'error': error_msg}
+        return jsonify(response)
+    
+    _id = obs_dict['observation_id']
+    observation = obs_dict
     # Now do what we already learned in the notebooks about how to transform
     # a single observation into a dataframe that will work with a pipeline.
     obs = pd.DataFrame([observation], columns=columns).astype(dtypes)
     # Now get ourselves an actual prediction of the positive class.
     proba = pipeline.predict_proba(obs)[0, 1]
-    response = {'proba': proba}
+    prediction = pipeline.predict(obs)
+    response = {'proba': bool(prediction)}
+    
     p = Prediction(
         observation_id=_id,
         proba=proba,
+        prediction= prediction,
         observation=request.data
     )
+
     try:
         p.save()
     except IntegrityError:
@@ -87,14 +125,20 @@ def predict():
     return jsonify(response)
 
 
-@app.route('/update', methods=['POST'])
+@app.route('/search_result', methods=['POST'])
 def update():
     obs = request.get_json()
     try:
-        p = Prediction.get(Prediction.observation_id == obs['id'])
-        p.true_class = obs['true_class']
+        p = Prediction.get(Prediction.observation_id == obs['observation_id'])
+        p.true_class = obs['outcome']
         p.save()
-        return jsonify(model_to_dict(p))
+        return jsonify(
+                {
+                    "observation_id": p.observation_id,
+                    "outcome": p.true_class,
+                    "predicted_outcome": p.prediction
+                }
+            )
     except Prediction.DoesNotExist:
         error_msg = 'Observation ID: "{}" does not exist'.format(obs['id'])
         return jsonify({'error': error_msg})
